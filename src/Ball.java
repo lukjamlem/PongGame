dbe7b527807c8ef020015eae55cfe5c4f6aeccce
Łukasz Jamrozik <lukjamlem@gmail.com>
2023-02-28 18:52:35 +0100

    add main and classes

---
+++ src/Ball.java
@@ -0,0 +1,27 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+
+public class Ball extends Rectangle{
+    Random random;
+    int xVelocity;
+    int yVelocity;
+
+    Ball() {
+
+    }
+
+    public void setXdirection(int randomXDirection) {
+
+    }
+    public void setYdirection(int randomYDirection) {
+
+    }
+    public void move() {
+
+    }
+    public void draw(Graphics g) {
+
+    }
+}
---
+++ src/PongGame.java
@@ -0,0 +1,12 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+
+public class PongGame {
+
+    public static void main (String[] args) {
+        //System.out.println("Hello Game");
+        GameFrame frame = new GameFrame();
+    }
+}
